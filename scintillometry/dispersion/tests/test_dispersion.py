d9572d3a834bae9543b29e44ffb10755c04125dc
Chenchong Zhu <chenchong.zhu@gmail.com>
2018-07-03 14:22:13 -0400

    Addressed PR comments.

--- scintillometry/dispersion/tests/test_dispersion.py
+++ scintillometry/dispersion/tests/test_dispersion.py
@@ -24,7 +24,7 @@ class TestDM(object):
         assert dm_differentunit == dm
         with pytest.raises(u.UnitsError) as excinfo:
             DispersionMeasure(self.dm_val * u.s)
-        assert "dispersion measures should have units" in str(excinfo.value)
+        assert "require units equivalent to 'pc / cm3'" in str(excinfo.value)
         # Check that we can copy (and the new object is the same class).
         dm_copy = dm.copy()
         assert dm_copy == dm.copy()
@@ -32,6 +32,14 @@ class TestDM(object):
 
     def test_calculation(self):
         """Test time and phase offset calculation."""
+        # Simple test with dm = 1, freq = 1 * u.MHz.
+        dm = DispersionMeasure(1.)
+        assert dm.time_delay(1. * u.MHz) == 1. / 2.41e-4 * u.s
+        phase_delay = (1. / 2.41e-4) * (u.cycle * u.MHz * u.s)
+        assert dm.phase_delay(1. * u.MHz) == phase_delay
+        assert np.isclose(dm.phase_factor(1. * u.MHz),
+                          np.exp(1j * phase_delay.to_value(u.rad)), rtol=1e-6)
+
         # Array of random frequencies within 64 MHz of the reference frequency.
         freqs = np.array([369.66462, 373.56482, 319.541562,
                           297.2516, 321.053234]) * u.MHz
@@ -41,19 +49,19 @@ class TestDM(object):
         time_delay = (dm.dispersion_delay_constant * dm *
                       (1. / freqs**2 - 1. / ref_freq**2))
         assert_quantity_allclose(dm.time_delay(freqs, ref_freq),
-                                 time_delay, rtol=1e-12)
+                                 time_delay, rtol=1e-13)
         time_delay_infref = dm.dispersion_delay_constant * dm / freqs**2
         assert_quantity_allclose(dm.time_delay(freqs),
-                                 time_delay_infref, rtol=1e-12)
+                                 time_delay_infref, rtol=1e-13)
 
         phase_delay = (2. * np.pi * u.rad * dm.dispersion_delay_constant * dm *
                        freqs * (1. / ref_freq - 1. / freqs)**2)
         assert_quantity_allclose(dm.phase_delay(freqs, ref_freq),
-                                 phase_delay, rtol=1e-12)
+                                 phase_delay, rtol=1e-13)
         phase_delay_infref = (2. * np.pi * u.rad *
                               dm.dispersion_delay_constant * dm * 1. / freqs)
         assert_quantity_allclose(dm.phase_delay(freqs),
-                                 phase_delay_infref, rtol=1e-12)
+                                 phase_delay_infref, rtol=1e-13)
 
         phase_factor = np.exp(1j * phase_delay.to_value(u.rad))
         # Not great rtol, since we used np.exp() and phase factors are large
