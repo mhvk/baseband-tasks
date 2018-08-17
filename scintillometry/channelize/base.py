41c994ea73a306101fcd5e4e7cf318685369209b
Chenchong Zhu <chenchong.zhu@gmail.com>
2018-08-17 17:26:26 -0400

    Addressed PR comments

--- scintillometry/channelize/base.py
+++ scintillometry/channelize/base.py
@@ -13,36 +13,27 @@ class ChannelizeTask(TaskBase):
 
     def __init__(self, ih, nchan, samples_per_frame=1, FFT=None):
 
-        super().__init__(ih)
-
         self.nchan = operator.index(nchan)
         # NOTE: should this be made private?  Should the Channelizer at least
         # have a _min_samples_per_frame value?
-        self.samples_per_frame = operator.index(samples_per_frame)
-        # NOTE: nsample is the number of output samples for the largest integer
-        # number of frames available from the input file.
-        self._nsample = self.samples_per_frame * (
-            self.ih.shape[0] // self.nchan // self.samples_per_frame)
-        assert self._nsample > 0, ("not enough samples to fill one frame of "
-                                   "data!")
-        self.sample_rate = ih.sample_rate / self.nchan
+        samples_per_frame = operator.index(samples_per_frame)
+        sample_rate = ih.sample_rate / self.nchan
+
+        nsample = samples_per_frame * (
+            ih.shape[0] // self.nchan // samples_per_frame)
+        assert nsample > 0, "not enough samples to fill one frame of data!"
+        self._raw_frame_len = self.nchan * samples_per_frame
 
         # Initialize channelizer.
         if FFT is None:
             FFT = get_fft_maker('numpy')
-        self._fft = FFT(
-            (self.samples_per_frame, self.nchan) + self.ih.sample_shape,
-            self.ih.dtype, axis=1)
+        self._fft = FFT((samples_per_frame, self.nchan) + ih.sample_shape,
+                        ih.dtype, axis=1)
+
+        super().__init__(ih, (nsample,) + self._fft.freq_shape[1:],
+                         sample_rate, samples_per_frame, self._fft.freq_dtype)
 
     def _read_frame(self, frame_index):
-        self.ih.seek(frame_index * self.nchan * self.samples_per_frame)
-        data = self.ih.read(self.nchan * self.samples_per_frame)
+        self.ih.seek(frame_index * self._raw_frame_len)
+        data = self.ih.read(self._raw_frame_len)
         return self._fft(data.reshape(self._fft.time_shape))
-
-    @property
-    def dtype(self):
-        return self._fft.freq_dtype
-
-    @property
-    def sample_shape(self):
-        return self._fft.freq_shape[1:]
