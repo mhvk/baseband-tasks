44ba9f0c4b0e7a4a6b723a1ae8324ae65d0a2f1e
Chenchong Zhu <chenchong.zhu@gmail.com>
2018-08-17 13:38:45 -0400

    WIP created squaring module.

---
+++ scintillometry/squaring/base.py
@@ -0,0 +1,25 @@
+# Licensed under the GPLv3 - see LICENSE
+
+import numpy as np
+
+from ..base.base import TaskBase
+
+
+__all__ = ['SquareTask']
+
+
+class SquareTask(TaskBase):
+
+    def __init__(self, ih, samples_per_frame=1):
+        ih_dtype = np.dtype(ih.dtype)
+        if ih_dtype.kind == 'f':
+            dtype = ih_dtype
+        else:
+            dtype = np.dtype('f{0:d}'.format(ih_dtype.itemsize // 2))
+        super().__init__(ih, ih.shape, ih.sample_rate, ih.samples_per_frame,
+                         dtype)
+
+    def _read_frame(self, frame_index):
+        self.ih.seek(frame_index * self.samples_per_frame)
+        data = self.ih.read(self.samples_per_frame)
+        return np.real(data * np.conj(data))
