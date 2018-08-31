0d3f064032ffecd3715dd8b00c11ca2a7ee2ef40
Chenchong Zhu <chenchong.zhu@gmail.com>
2018-08-31 14:23:28 -0400

    Add coherent dedispersion task.  No docs, docstrings or tests yet.

---
+++ scintillometry/dedisperse.py
@@ -0,0 +1,121 @@
+# Licensed under the GPLv3 - see LICENSE
+
+import numpy as np
+import astropy.units as u
+import operator
+from astropy.utils import lazyproperty
+
+from .base import TaskBase
+from .fourier import get_fft_maker
+from .dm import DispersionMeasure
+
+
+__all__ = ['CoherentDedispersionTask']
+
+
+class CoherentDedispersionTask(TaskBase):
+
+    def __init__(self, ih, dm, base_freq, pad_samples_per_frame,
+                 freq_order=None, FFT=None):
+
+        # Store the number of inputs samples to read per frame.
+        self._pad_samples_per_frame = operator.index(pad_samples_per_frame)
+
+        # Set reference frequency.  If not complex, base_freq is at the edge of
+        # the band, and freq_order determines which edge (1 for bottom, -1 for
+        # top).  If complex, base_freq is at the middle of the band.
+        # NOTE: unless the exact bandwidth edge is returned by np.fft.rfftfreq,
+        # this will give slightly different answers than Rob's code.
+        self._base_freq = np.atleast_1d(np.array(base_freq)) * base_freq.unit
+        # Set the frequency order (1 for increasing, -1 for decreasing).
+        if freq_order is None:
+            freq_order = np.ones(ih.sample_shape, dtype=int)
+        self._freq_order = np.atleast_1d(np.array(freq_order))
+
+        half_rate = ih.sample_rate.value / 2.
+        if not ih.complex_data:
+            bandwidth = np.array([0., half_rate]) * ih.sample_rate.unit
+        else:
+            bandwidth = np.array([-half_rate, half_rate]) * ih.sample_rate.unit
+        extrema_freqs = self.base_freq + (self.freq_order *
+                                          bandwidth[:, np.newaxis])
+        self._max_freq = np.max(extrema_freqs)
+
+        # Set dispersion measure.
+        self.dm = DispersionMeasure(dm)
+
+        # Calculate maximum time delay, and determine the size of padding
+        # needed to account for dispersion.
+        max_time_delay = self.dm.time_delay(np.min(extrema_freqs),
+                                            self.max_freq)
+        max_delay_offset = int(np.ceil((max_time_delay *
+                                        ih.sample_rate).to_value(u.one)))
+        samples_per_frame = self._pad_samples_per_frame - max_delay_offset
+        assert samples_per_frame > 0, (
+            "pad_samples_per_frame={} is smaller than the minimum number "
+            "needed for dedispersion, {}".format(pad_samples_per_frame,
+                                                 max_delay_offset + 1))
+
+        # Calculate task output shape and sample rate.
+        nframe, remaining_samples = divmod(ih.shape[0], samples_per_frame)
+        # HACK: We don't support partial frames yet, so if nframe > 1 but
+        # remaining_samples < max_delay_offset, we reduce the number of frames
+        # by an appropriate amount to prevent EOF errors.
+        if nframe > 1 and remaining_samples < max_delay_offset:
+            if samples_per_frame > max_delay_offset:
+                nframe -= 1
+                remaining_samples += samples_per_frame
+            else:
+                subtract_nframes = max_delay_offset // samples_per_frame + 1
+                nframe -= subtract_nframes
+                remaining_samples += samples_per_frame * subtract_nframes
+        assert nframe > 0, "not enough samples to fill one frame of data!"
+        self._nsample = samples_per_frame * nframe
+
+        # Initialize FFT.
+        if FFT is None:
+            FFT = get_fft_maker('numpy')
+        # Dedispersion FFT and inverse.
+        self._fft = FFT((self._pad_samples_per_frame,) + ih.sample_shape,
+                        self.ih.dtype, axis=0, sample_rate=self.ih.sample_rate)
+        self._ifft = self._fft.inverse()
+
+        super().__init__(ih, (self._nsample,) + ih.sample_shape,
+                         ih.sample_rate, samples_per_frame, ih.dtype)
+
+    @property
+    def base_freq(self):
+        return self._base_freq
+
+    @property
+    def max_freq(self):
+        return self._max_freq
+
+    @property
+    def freq_order(self):
+        return self._freq_order
+
+    @lazyproperty
+    def freq(self):
+        return self.base_freq + self.freq_order * self._fft.freq
+
+    @lazyproperty
+    def phase_factor(self):
+        phase_factor = self.dm.phase_factor(self.freq, self.max_freq)
+        phase_factor[:, self.freq_order == 1] = (
+            np.conj(phase_factor[:, self.freq_order == 1]))
+        return phase_factor
+
+    def dedisperse(self, data):
+        return self._ifft(self._fft(data) *
+                          self.phase_factor)[:self._samples_per_frame]
+
+    def _read_frame(self, frame_index):
+        # TODO: For speed during sequential reading, can buffer frames, then,
+        # during sequential decode, copy data from self._samples_per_frame to
+        # self._pad_samples_per_frame into the next buffer, rather than
+        # re-decoding them.  Dangerous if self.ih gets manually shifted
+        # between reads.
+        self.ih.seek(frame_index * self._samples_per_frame)
+        data = self.ih.read(self._pad_samples_per_frame)
+        return self.dedisperse(data)
