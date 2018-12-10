db78836f76a5c92e142836533e1e5a4b6bece4ce
Jing Luo <luojing1211@gmail.com>
2018-12-10 17:52:51 -0500

    Add pint utils test

---
+++ scintillometry/tests/test_pintutils.py
@@ -0,0 +1,7 @@
+# Licensed under the GPLv3 - see LICENSE
+"""Full-package tests of pint_utils sources."""
+
+import pytest
+import numpy as np
+import astropy.units as u
+from astropy.time import Time
