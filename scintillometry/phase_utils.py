61b2b7171a160f19fcb3dcb6b4a74b5e0fda9d72
Jing Luo <luojing1211@gmail.com>
2018-12-12 12:22:39 -0500

    add apparent freqeuency

--- scintillometry/phase_utils.py
+++ scintillometry/phase_utils.py
@@ -12,7 +12,7 @@ except:
     pass
 from .predictor import Polyco
 
-__all__ = ['PhaseBase']
+__all__ = ['PintPhase', 'PolycoPhase']
 
 
 class PintPhase(object):
@@ -32,7 +32,8 @@ class PintPhase(object):
 
        Note
        ----
-       This method provides high precision phase calculation.
+       This method provides high precision phase calculation. The fractional
+       phase is between -0.5 to 0.5.
     """
     def __init__(self, par_file, obs, obs_freq, solar_ephem='de436',
                  bipm_version='BIPM2015'):
@@ -44,20 +45,85 @@ class PintPhase(object):
         self.bipm_version = bipm_version
 
     def __call__(self, t):
+        """Compute the apparent phase at one or more time stemp
+
+           Parameter
+           ---------
+           t : `astropy.time.Time` object
+               The input time stemps.
+
+           Return
+           ------
+           The apparent pulse phase at time t. The phases are return as
+           'integer phase, fractional phase'
+        """
         self.pu.get_toas(time=t, obs=self.obs, obs_freq=self.obs_freq,
                          solar_ephem=self.solar_ephem,
                          bipm_version=self.bipm_version)
         ph = self.pu.compute_phase()
-        return (ph.int, ph.frac)
+        return u.Quantity((ph.int, ph.frac), u.cycle)
+
+    def apparent_spin_freq(self, t):
+        """Compute the apparent spin frequency at one or more time stemps.
+
+            Parameter
+            ---------
+            t : `astropy.time.Time` object
+                The input time stemps.
+
+            Return
+            ------
+            The apparent spin frequency at time t. The phases are return as a
+            tuple with '(integer phase, fractional phase)'.
+        """
+        self.pu.get_toas(time=t, obs=self.obs, obs_freq=self.obs_freq,
+                         solar_ephem=self.solar_ephem,
+                         bipm_version=self.bipm_version)
+        apprnt_f0 = self.pu.model.d_phase_d_toa(self.pu.toas)
+        return apprnt_f0
 
 
 class PolycoPhase(object):
-    """A utility class for a Tempo style polyco phase calculation
+    """A utility class for a Tempo style polyco phase calculation.
+
+       Parameter
+       ---------
+       polyco_file : str
+           Tempo style polyco file.
     """
     def __init__(self, polyco_file):
         self.polyco = Polyco(polyco_file)
 
     def __call__(self, t):
+        """Compute the apparent phase at one or more time stemp
+
+           Parameter
+           ---------
+           t : `astropy.time.Time` object
+               The input time stemps.
+
+           Return
+           ------
+           The apparent pulse phase at time t. The phases are return as
+           'integer phase, fractional phase'.
+        """
         t = t._apply(np.atleast_1d)
         ph = self.polyco(t)
-        return np.divmod(ph, 1 * u.cycle)
+        return u.Quantity(np.divmod(ph, 1), u.cycle)
+
+    def apparent_spin_freq(self, t):
+        """Compute the apparent spin frequency at one or more time stemps.
+
+            Parameter
+            ---------
+            t : `astropy.time.Time` object
+                The input time stemps.
+
+            Return
+            ------
+            The apparent spin frequency at time t. The phases are return as a
+            tuple with '(integer phase, fractional phase)'.
+        """
+        t = t._apply(np.atleast_1d)
+        apprnt_f0 = self.polyco(t, deriv=1)
+        return apprnt_f0.to(u.Hz, equivalencies=[(u.cy/u.s, u.Hz)])
