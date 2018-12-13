47b9181d19387995a8458ccc54a598dda577141a
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-12-13 11:45:36 -0500

    Clean up tests a bit, adding comments for clarity.

--- scintillometry/tests/test_integration.py
+++ scintillometry/tests/test_integration.py
@@ -23,12 +23,12 @@ class TestFoldBase:
                                        samples_per_frame=200, dtype=np.float)
         self.sh = Task(self.eh, self.pulse_simulate)
         self.period_bin = 125
+        self.F0 = 1.0 / (self.period_bin / self.sh.sample_rate)
         self.n_phase = 50
 
     def phase(self, t):
         dt = (t - self.start_time).to(u.s)
-        F0 = 1.0 / (self.period_bin / self.sh.sample_rate)
-        return F0 * dt
+        return self.F0 * dt
 
     def pulse_simulate(self, fh, data):
         idx = fh.tell() + np.arange(data.shape[0])
@@ -45,47 +45,47 @@ class TestFold(TestFoldBase):
         # Check if the input data is set up right.
         assert len(pulses) == 8, "Pulses are not simulated right."
 
-    def test_under_period(self):
-        # Test when folding time smaller than the pulse period.
+    def test_fold_time_shorter_than_period(self):
         fold_time = 11 * u.ms
-        eff_n_phase = (11 * u.ms / (self.period_bin / self.sh.sample_rate) *
-                       self.n_phase).to(u.Unit(1))
         self.fh = Fold(self.sh, self.n_phase, self.phase, fold_time,
-                       samples_per_frame=1)
+                       samples_per_frame=1, average=False)
         self.fh.seek(0)
         fr = self.fh.read(3)
+        max_count = int(np.ceil(
+            (self.sample_rate / self.F0 / self.n_phase).to_value(1)))
+        assert np.all(fr.count <= max_count)
 
+        # For each of the samples, check that the correct ones have not
+        # gotten any data.
+        eff_n_phase = (fold_time * self.F0 * self.n_phase).to_value(1)
         for ii, count in enumerate(fr.count):
-            u_eff_phb = np.where(count == 0)
-            assert np.isclose(self.n_phase - len(u_eff_phb[0]) / 8,
-                              eff_n_phase, 1), \
-                ("Sample {}'s does not have correct effective phase bin "
-                 "number.".format(ii))
+            n_count_0 = (count == 0).sum()
+            assert np.isclose(self.n_phase - n_count_0, eff_n_phase, 1), \
+                ("Sample {} has the wrong number of zero-count phase bins"
+                 .format(ii))
 
-    def test_over_period(self):
-        # Test when folding time is bigger than one or multiple pulse period
+    def test_fold_time_longer_than_period(self):
         fold_time = 26 * u.ms
         self.fh = Fold(self.sh, self.n_phase, self.phase, fold_time,
-                       samples_per_frame=1)
+                       samples_per_frame=1, average=False)
         self.fh.seek(0)
         fr = self.fh.read(10)
         # Compare the total counts of all the samples.
         tot_counts = np.sum(fr.count, axis=1)
-        abs_diff = np.abs(tot_counts - tot_counts.mean())
-        assert abs_diff.max() <= 1, ("Folding counts are not correct for over "
-                                     "period folding.")
-        # Test the output result
+        assert np.all(np.abs(tot_counts - tot_counts.mean()) <= 1), \
+            "Folding counts vary more than expected."
+        # Test the output on and off gates.
         ph0_bins = [0, 1, -1, -2]
         pulse_power = np.sum(fr[:, ph0_bins, 0, 0], axis=1)
-        assert np.logical_and(pulse_power[0] > 30, pulse_power[0] < 33), \
-            "Folding power is not correct for over period folding."
+        assert 30 < pulse_power[0] < 33, \
+            "Folded power of on-gate is incorrect."
 
-        assert np.logical_and(np.all(pulse_power[1:] > 20),
-                              np.all(pulse_power[1:] < 23)), \
-            "Folding power is not correct for over period folding."
-        # Test average
+        assert np.all((pulse_power[1:] > 20) & (pulse_power[1:] < 23)), \
+            "Folding power of off-gates is incorrect."
+        # Test averaging
         self.fh2 = Fold(self.sh, self.n_phase, self.phase, fold_time,
                         samples_per_frame=20, average=True)
         self.fh2.seek(0)
         fr2 = self.fh2.read(10)
-        assert np.all(fr2[:, 2:-1] == 0.125), "Averaged result is not correct."
+        assert np.all(fr2[:, 2:-1] == 0.125), \
+            "Average off-gate power is incorrect."
