c05058c457ac8c6010fd6b552e428f335c484920
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-03-31 20:41:35 -0400

    FFT variant of convolution.

--- scintillometry/convolution.py
+++ scintillometry/convolution.py
@@ -5,13 +5,14 @@ import warnings
 import numpy as np
 
 from .base import BaseTaskBase
+from .fourier import get_fft_maker
 
 
-__all__ = ['Convolve']
+__all__ = ['Convolve', 'FFTConvolve']
 
 
 class Convolve(BaseTaskBase):
-    """Conolve a time stream with a given filter.
+    """Convolve a time stream with a given filter.
 
     Parameters
     ----------
@@ -29,6 +30,10 @@ class Convolve(BaseTaskBase):
         output convolved samples per frame will be smaller to avoid wrapping.
         If not given, the minimum power of 2 needed to get at least 75%
         efficiency.
+
+    See Also
+    --------
+    FFTConvolve : convolution via a Fourier transform
     """
 
     def __init__(self, ih, response, offset=0, samples_per_frame=None):
@@ -54,8 +59,8 @@ class Convolve(BaseTaskBase):
         samples_per_frame -= pad
         shape = (ih.shape[0] - pad,) + ih.sample_shape
         super().__init__(ih, shape=shape, samples_per_frame=samples_per_frame)
-        self._pad_start = offset
-        self._pad_end = pad - offset
+        self._pad_start = pad - offset
+        self._pad_end = offset
         self._start_time += self._pad_start / ih.sample_rate
         self._response = np.broadcast_to(response, (response.shape[0],) +
                                          self.sample_shape)
@@ -75,3 +80,56 @@ class Convolve(BaseTaskBase):
         data = self.ih.read(self.samples_per_frame +
                             self._pad_start + self._pad_end)
         return self.task(data)
+
+
+class FFTConvolve(Convolve):
+    """Convolve a time stream with a given filter.
+
+    The convolution is done via multiplication in the Fourier domain.
+    For all but very simple responses, this faster than direct convolution.
+
+    Parameters
+    ----------
+    ih : task or `baseband` stream reader
+        Input data stream, with time as the first axis.
+    response : `~numpy.ndarray`
+        Response to convolce the time stream with.  If one-dimensional, assumed
+        to apply to the sample axis of ``ih``.
+    offset : int, optional
+        Where samples should be considered to be taken from.  For the default
+        of 0, a given sample has the same time as the convolution of the filter
+        with all preceding samples.
+    samples_per_frame : int, optional
+        Number of samples which should be convolved in one go. The number of
+        output convolved samples per frame will be smaller to avoid wrapping.
+        If not given, the minimum power of 2 needed to get at least 75%
+        efficiency.
+    FFT : FFT maker or None, optional
+        FFT maker.  Default: `None`, in which case the channelizer uses the
+        default from `~scintillometry.fourier.base.get_fft_maker` (pyfftw if
+        available, otherwise numpy).
+
+    See Also
+    --------
+    Convolve : direct convolution.
+    """
+    def __init__(self, ih, response, offset=0, samples_per_frame=None,
+                 FFT=None):
+        super().__init__(ih, response=response, offset=offset,
+                         samples_per_frame=samples_per_frame)
+        # Initialize FFTs for fine channelization and the inverse.
+        if FFT is None:
+            FFT = get_fft_maker()
+
+        raw_frame_shape = (samples_per_frame,) + ih.sample_shape
+        self._fft = FFT(shape=raw_frame_shape, sample_rate=ih.sample_rate,
+                        dtype=ih.dtype)
+        self._ifft = self._fft.inverse()
+        long_response = np.zeros(raw_frame_shape, ih.dtype)
+        long_response[:self._response.shape[0]] = self._response
+        self._ft_response = self._fft(long_response)
+
+    def task(self, data):
+        ft = self._fft(data)
+        ft *= self._ft_response
+        return self._ifft(ft)[self._pad_start:data.shape[0]-self._pad_end]
--- scintillometry/tests/test_convolution.py
+++ scintillometry/tests/test_convolution.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_array_equal
 import astropy.units as u
 from astropy.time import Time
 
-from ..convolution import Convolve
+from ..convolution import Convolve, FFTConvolve
 from ..generators import EmptyStreamGenerator
 
 from baseband import vdif, dada
@@ -16,30 +16,31 @@ from baseband.data import SAMPLE_VDIF, SAMPLE_DADA
 class TestConvolve:
     """Test convolution with simple smoothing filter."""
 
-    Convolve = Convolve
-
     def setup(self):
         self.response = np.ones(3)
 
-    def test_convolve(self):
+    @pytest.mark.parametrize('convolve_task', (Convolve, FFTConvolve))
+    def test_convolve(self, convolve_task):
         # Load baseband file and get reference intensities.
         fh = dada.open(SAMPLE_DADA)
         ref_data = fh.read()
         expected = ref_data[:-2] + ref_data[1:-1] + ref_data[2:]
 
         # Have 16000 - 2 useful samples -> can use 842, but add 2 for response.
-        ct = self.Convolve(fh, self.response, samples_per_frame=844)
+        ct = convolve_task(fh, self.response, samples_per_frame=844)
         # Convolve everything.
         data1 = ct.read()
-        assert ct.start_time == fh.start_time
         assert ct.tell() == ct.shape[0] == fh.shape[0] - 2
-        assert np.allclose(expected, data1)
+        assert abs(ct.start_time - fh.start_time -
+                   2 / fh.sample_rate) < 1. * u.ns
+        assert abs(ct.stop_time - fh.stop_time) < 1. * u.ns
+        assert np.allclose(expected, data1, atol=1.e-4)
 
         # Seeking and selective convolution.
         ct.seek(-3, 2)
         assert ct.tell() == ct.shape[0] - 3
         data2 = ct.read()
         assert data2.shape[0] == 3
-        assert np.allclose(expected[-3:], data2)
+        assert np.allclose(expected[-3:], data2, atol=1.e-4)
 
         ct.close()
