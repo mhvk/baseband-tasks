f040b9ba93d6e83a0f1fb257cc162023155ffe65
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-04-08 22:13:05 -0400

    Add inverse PFB - it doesn't work well near edges.
    This seems unavoidable. Earlier tests did not properly take into
    account that one really looses data.

--- baseband_tasks/pfb.py
+++ baseband_tasks/pfb.py
@@ -4,7 +4,7 @@ from astropy.utils import lazyproperty
 
 
 from .base import PaddedTaskBase
-from .channelize import Channelize
+from .channelize import Channelize, Dechannelize
 
 
 __all__ = ['PolyphaseFilterBankSamples', 'PolyphaseFilterBank']
@@ -45,6 +45,8 @@ def sinc_hamming(n_tap, n_sample, sinc_scale=1.):
 class PolyphaseFilterBankSamples(Channelize):
     """Channelize a time stream using a polyphase filter bank.
 
+    This version applies the polyphase filter in the time domain.
+
     Parameters
     ----------
     ih : task or `baseband` stream reader
@@ -68,6 +70,9 @@ class PolyphaseFilterBankSamples(Channelize):
         if samples_per_frame is not None:
             samples_per_frame = samples_per_frame * n
         assert pad % 2 == 0
+        # Note: cannot easily use Convolve since this doesn't convolve
+        # every sample, but rather each n_chan samples.  In principle,
+        # it could be done via a ReshapeSamples followed by a Convolve.
         self.padded = PaddedTaskBase(ih, pad_start=pad//2, pad_end=pad//2,
                                      samples_per_frame=samples_per_frame)
         self.padded.task = self.ppf
@@ -89,6 +94,26 @@ class PolyphaseFilterBankSamples(Channelize):
 
 
 class PolyphaseFilterBank(PolyphaseFilterBankSamples):
+    """Channelize a time stream using a polyphase filter bank.
+
+    This version applies the polyphase filter in the frequency domain.
+
+    Parameters
+    ----------
+    ih : task or `baseband` stream reader
+        Input data stream, with time as the first axis.
+    response : `~numpy.ndarray`
+        Polyphase filter.  The first dimension is taken to be the
+        number of taps, and the second the number of channels.
+    samples_per_frame : int, optional
+        Number of complete output samples per frame (see Notes).  Default: 1.
+    frequency : `~astropy.units.Quantity`, optional
+        Frequencies for each channel in ``ih`` (channelized frequencies will
+        be calculated).  Default: taken from ``ih`` (if available).
+    sideband : array, optional
+        Whether frequencies in ``ih`` are upper (+1) or lower (-1) sideband.
+        Default: taken from ``ih`` (if available).
+    """
     def __init__(self, ih, response, samples_per_frame=None,
                  frequency=None, sideband=None):
         super().__init__(ih, response=response,
@@ -114,4 +139,50 @@ class PolyphaseFilterBank(PolyphaseFilterBankSamples):
         # Remove padding, which has turned around.
         result = result[:result.shape[0]+1-self._response.shape[0]]
         # Reshape as timestream (channelizer will immediately undo this...).
+        return result.reshape((-1,) + result.shape[2:])
+
+
+class InversePolyphaseFilterBank(PaddedTaskBase):
+    def __init__(self, ih, response, sn, n=None, samples_per_frame=None,
+                 frequency=None, sideband=None, dtype=None):
+        self.dechannelized = Dechannelize(
+            ih, n=n, samples_per_frame=None, frequency=frequency,
+            sideband=sideband, dtype=dtype)
+        self._FFT = self.dechannelized._FFT
+        n_tap, n = response.shape
+        pad = (n_tap - 1) * n
+        if samples_per_frame is not None:
+            samples_per_frame = samples_per_frame + pad
+        assert pad % 2 == 0
+        super().__init__(self.dechannelized, pad_start=pad//2, pad_end=pad//2,
+                         samples_per_frame=samples_per_frame)
+        self._response = response
+        self._reshape = ((self._ih_samples_per_frame // n, n)
+                         + self.ih.sample_shape)
+        self._ppf_fft = self._FFT(
+            shape=self._reshape, dtype=self.dtype)
+        self._ppf_ifft = self._ppf_fft.inverse()
+        self._inv_sn2 = 1. / (sn * sn)
+
+    @lazyproperty
+    def _ft_inverse_response(self):
+        long_response = np.zeros(self._reshape[:2], self.dtype)
+        long_response[:self._response.shape[0]] = self._response
+        long_response.shape = (long_response.shape
+                               + (1,) * len(self.ih.sample_shape))
+        fft = self._FFT(shape=long_response.shape, dtype=self.ih.dtype)
+        ft_response = fft(long_response).conj()
+        inverse = (ft_response.conj()
+                   / (ft_response.real ** 2 + ft_response.imag ** 2
+                      + self._inv_sn2))
+        return inverse
+
+    def task(self, data):
+        data = data.reshape(self._reshape)
+        ft = self._ppf_fft(data)
+        ft *= self._ft_inverse_response
+        result = self._ppf_ifft(ft)
+        # Remove padding.
+        result = result[self._response.shape[0]-1:]
+        # Reshape as timestream
         return result.reshape((-1,) + result.shape[2:])
--- baseband_tasks/tests/test_pfb.py
+++ baseband_tasks/tests/test_pfb.py
@@ -2,6 +2,7 @@
 """Tests of the polyphase filterbank module."""
 import os
 
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 
@@ -10,7 +11,8 @@ from astropy.time import Time
 
 from baseband_tasks.generators import NoiseGenerator
 from baseband_tasks.pfb import (
-    sinc_hamming, PolyphaseFilterBankSamples, PolyphaseFilterBank)
+    sinc_hamming, PolyphaseFilterBankSamples, PolyphaseFilterBank,
+    InversePolyphaseFilterBank)
 
 
 test_data = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -29,14 +31,14 @@ class TestSincHamming:
 
 class TestBasics:
     def setup(self):
-        self.nh = NoiseGenerator(shape=(32 * 2048,),
+        self.nh = NoiseGenerator(shape=(2500 * 2048,),
                                  start_time=Time('2010-01-01'),
                                  sample_rate=1.*u.kHz, seed=12345,
-                                 samples_per_frame=8, dtype='f8')
-        self.nc = NoiseGenerator(shape=(32 * 2048,),
+                                 samples_per_frame=128, dtype='f8')
+        self.nc = NoiseGenerator(shape=(2500 * 2048,),
                                  start_time=Time('2010-01-01'),
                                  sample_rate=1.*u.kHz, seed=12345,
-                                 samples_per_frame=8, dtype='c16')
+                                 samples_per_frame=128, dtype='c16')
         self.n_tap = 4
         self.n_chan = 2048
         n = self.n_tap * self.n_chan
@@ -44,7 +46,8 @@ class TestBasics:
         x = r * np.linspace(-0.5, 0.5, n, endpoint=False)
         self.pfb = (np.sinc(x) * np.hamming(4 * 2048)).reshape(4, 2048)
 
-    def test_understanding(self):
+    @pytest.mark.parametrize('offset', (0, 1000))
+    def test_understanding(self, offset):
         """Stepping or frequency selection should give same answer.
 
         Often think of PFB as multiplying with an array that is, e.g.,
@@ -53,7 +56,7 @@ class TestBasics:
         But this is equivalent to, after multiplication, summing 4
         subsequent arrays and taking the FT of that.
         """
-        self.nh.seek(0)
+        self.nh.seek(offset * 2048)
         # First check for real data.
         d = self.nh.read(5 * 2048).reshape(-1, 2048)
         hd = self.pfb * d[:4]
@@ -63,16 +66,19 @@ class TestBasics:
 
         # Check actual implementations.
         pfb = PolyphaseFilterBankSamples(self.nh, self.pfb)
+        pfb.seek(offset)
         ft_pfb = pfb.read(2)
         assert_allclose(ft_pfb[0], ft2_hd)
         assert_allclose(ft_pfb[1], np.fft.rfft((self.pfb * d[1:]).sum(0)))
         pfb2 = PolyphaseFilterBank(self.nh, self.pfb)
+        pfb2.seek(offset)
         ft_pfb2 = pfb2.read(2)
         assert_allclose(ft_pfb2, ft_pfb)
 
-    def test_understanding_complex(self):
+    @pytest.mark.parametrize('offset', (0, 1000))
+    def test_understanding_complex(self, offset):
         # Check above holds for complex too.
-        self.nc.seek(0)
+        self.nc.seek(offset * 2048)
         c = self.nc.read(4 * 2048).reshape(-1, 2048)
         hc = self.pfb * c
         ft1_hc = np.fft.fft(hc.ravel())[::4]
@@ -81,8 +87,40 @@ class TestBasics:
 
         # And check actual implementation.
         pfb = PolyphaseFilterBankSamples(self.nc, self.pfb)
+        pfb.seek(offset)
         ft_pfb = pfb.read(1)[0]
         assert_allclose(ft_pfb, ft2_hc)
-        pfb = PolyphaseFilterBank(self.nc, self.pfb)
-        ft_pfb = pfb.read(1)[0]
-        assert_allclose(ft_pfb, ft2_hc)
+        pfb2 = PolyphaseFilterBank(self.nc, self.pfb)
+        pfb2.seek(offset)
+        ft_pfb2 = pfb2.read(1)[0]
+        assert_allclose(ft_pfb2, ft2_hc)
+
+    def test_inversion_understanding(self):
+        n_sample = 128
+        self.nh.seek(3 * 2048)
+        d_in = self.nh.read(n_sample * 2048).reshape(-1, 2048)
+        pfb = PolyphaseFilterBank(self.nh, self.pfb,
+                                  samples_per_frame=n_sample)
+        ft_pfb = pfb.read(n_sample+3)
+        # Dechannelize.
+        d_pfb = np.fft.irfft(ft_pfb, axis=1)
+        # Deconvolve.
+        ft_dec = np.fft.rfft(d_pfb, axis=0)
+        ft_dec /= pfb._ft_response_conj
+        d_out = np.fft.irfft(ft_dec, axis=0, n=d_pfb.shape[0])
+        d_out = d_out[3:]
+        # We cannot hope to deconvolve near the edges, which means we loose
+        # the middle samples.
+        assert_allclose(d_in[32:-32, :950], d_out[32:-32, :950], atol=0.01)
+        assert_allclose(d_in[32:-32, 1100:], d_out[32:-32, 1100:], atol=0.01)
+
+    def test_inversion(self):
+        self.nh.seek(3 * 2048)
+        d_in = self.nh.read(128 * 2048).reshape(-1, 2048)
+        pfb = PolyphaseFilterBank(self.nh, self.pfb)
+        ipfb = InversePolyphaseFilterBank(pfb, self.pfb, sn=1e9, n=2048,
+                                          samples_per_frame=128*2048,
+                                          dtype=self.nh.dtype)
+        d_out = ipfb.read(128 * 2048).reshape(-1, 2048)
+        assert_allclose(d_in[32:-32, :950], d_out[32:-32, :950], atol=0.01)
+        assert_allclose(d_in[32:-32, 1100:], d_out[32:-32, 1100:], atol=0.01)
