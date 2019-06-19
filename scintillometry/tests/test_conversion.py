8d6636881d1a5d8b7b63868725c046f87b719535
00rebe <00rebe@gmail.com>
2019-06-19 13:08:44 -0400

    Add tests for more sine frequencies:

--- scintillometry/tests/test_conversion.py
+++ scintillometry/tests/test_conversion.py
@@ -4,29 +4,23 @@
 
 import pytest
 import numpy as np
+from numpy.testing import assert_allclose
 import astropy.units as u
 from astropy.time import Time
 
 from ..conversion import Real2Complex
-from ..generators import StreamGenerator
-
-
-def real_delta(handle):
-
-    real_delta = np.zeros(handle.samples_per_frame, dtype=np.float64)
-    if handle.offset == 0:
-        real_delta[0] = 1.0
-    return real_delta
-
-
-def real_sine(handle):
-
-    real_sine = np.sin(np.pi * np.arange(handle.samples_per_frame) / 2)
-    return real_sine
+from ..generators import StreamGenerator, EmptyStreamGenerator
 
 
 def test_real_to_complex_delta():
     """Test converting a real delta function to complex."""
+
+    def real_delta(handle):
+        real_delta = np.zeros(handle.samples_per_frame, dtype=np.float64)
+        if handle.offset == 0:
+            real_delta[0] = 1.0
+        return real_delta
+
     delta_fh = StreamGenerator(real_delta,
                                samples_per_frame=1024,
                                start_time=Time('2010-11-12T13:14:15'),
@@ -47,8 +41,25 @@ def test_real_to_complex_delta():
     assert np.isclose(complex_signal, complex_delta).all()
 
 
-def test_real_to_complex_sine():
+def test_expected_failures():
+    with pytest.raises(ValueError):
+        Real2Complex(
+            EmptyStreamGenerator(samples_per_frame=1024,
+                                 start_time=Time('2010-11-12T13:14:15'),
+                                 sample_rate=1. * u.kHz,
+                                 shape=(2048, ),
+                                 dtype='c8'))
+
+
+@pytest.mark.parametrize('f_nyquist', (0.75, 0.5, 0.25, 0.125, 0.5 + 1 / 32))
+def test_real_to_complex_sine(f_nyquist):
     """Test converting a real sine function to complex."""
+
+    def real_sine(handle):
+
+        real_sine = np.sin(f_nyquist * np.pi *
+                           np.arange(handle.samples_per_frame))
+        return real_sine
 
     sine_fh = StreamGenerator(real_sine,
                               samples_per_frame=1024,
@@ -56,11 +67,14 @@ def test_real_to_complex_sine():
                               sample_rate=1. * u.kHz,
                               shape=(2048, ),
                               dtype='f8')
-    complex_dc = -1j * np.ones(2048 // 2, dtype=np.complex128)
+
+    f_complex = f_nyquist - 0.5
+    complex_dc = np.exp(2j * np.pi *
+                        (-0.25 + np.arange(2048 // 2) * f_complex))
 
     real2complex = Real2Complex(sine_fh)
     complex_signal = real2complex.read()
 
     assert complex_signal.shape == (1024, )
     assert np.iscomplexobj(complex_signal)
-    assert np.isclose(complex_signal, complex_dc).all()
+    assert_allclose(complex_signal, complex_dc, atol=1e-8)
