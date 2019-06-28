d8dc9e57ae4df5bb4e3b804c37e7c1652af0ab16
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-06-28 17:41:16 -0400

    Allow combine tasks to work just on time common to streams.

--- scintillometry/combining.py
+++ scintillometry/combining.py
@@ -1,5 +1,6 @@
 # Licensed under the GPLv3 - see LICENSE
 import numpy as np
+from astropy import units as u
 
 from .base import TaskBase, Task
 
@@ -20,22 +21,43 @@ class CombineStreamsBase(TaskBase):
     ----------
     ihs : tuple of task or `baseband` stream readers
         Input data streams.
+    atol : `~astropy.units.Quantity`
+        Tolerance in units of time within which streams should be considered
+        aligned.
+    samples_per_frame : int, optional
+        Number of samples which should be fed to the function in one go.
+        If not given, by default the number from the first underlying file.
+        Useful mostly in case the stream readers have time offsets, since
+        the output stream will be shortened by an integer number of frames.
     """
-    def __init__(self, ihs):
+    def __init__(self, ihs, atol=1.*u.ns, samples_per_frame=None):
         try:
             ih0 = ihs[0]
         except (TypeError, IndexError) as exc:
             exc.args += ("Need an iterable containing at least one stream.",)
             raise
 
-        # Check consistency of the streams.
+        # Check consistency of the streams, and determine common time.
         self.ihs = ihs
+        start_time = ih0.start_time
+        stop_time = ih0.stop_time
         for ih in ihs[1:]:
             assert ih.sample_rate == ih0.sample_rate
             assert ih.dtype == ih0.dtype
-            # TODO: lift this restriction?
-            assert ih.start_time == ih0.start_time
-            assert ih.shape[0] == ih0.shape[0]
+            start_time = max(start_time, ih.start_time)
+            stop_time = min(stop_time, ih.stop_time)
+
+        n_sample = ih0.seek(stop_time) - ih0.seek(start_time)
+
+        # Calculate offsets for each file and check they are aligned well.
+        # TODO: use future TimeInterpolate class to lift this restriction?
+        self._start_offsets = []
+        for ih in ihs:
+            offset = ih.seek(start_time)
+            if abs(ih.time - start_time) > atol:
+                raise ValueError("streams not aligned to within {}"
+                                 .format(atol))
+            self._start_offsets.append(offset)
 
         # Check that the stream samples can be combined.
         fakes = [np.empty((7,) + ih.sample_shape, ih.dtype) for ih in ihs]
@@ -49,10 +71,15 @@ class CombineStreamsBase(TaskBase):
             raise ValueError("stream combination affected the sample axis (0).")
 
         # Calculate the combined shape and attributes.
-        shape = (ih0.shape[0],) + a.shape[1:]
+        shape = (n_sample,) + a.shape[1:]
         attrs = {attr: self._combine_attr(attr)
                  for attr in ('frequency', 'sideband', 'polarization')}
-        super().__init__(ih0, shape=shape, **attrs)
+        super().__init__(ih0, shape=shape, samples_per_frame=samples_per_frame,
+                         **attrs)
+        # Could just pass in start_time above if we based ourselves on
+        # BaseTaskBase, which is more logical, but then cannot mixin Task
+        # below.  TODO: remove TaskBase intermediary?
+        self._start_time = start_time
 
     def _combine_attr(self, attr):
         """Combine the given attribute from all streams.
@@ -102,8 +129,8 @@ class CombineStreamsBase(TaskBase):
     def _read_frame(self, frame_index):
         """Read and combine data from the underlying filehandles."""
         data = []
-        for ih in self.ihs:
-            ih.seek(frame_index * self._samples_per_frame)
+        for ih, start_offset in zip(self.ihs, self._start_offsets):
+            ih.seek(start_offset + frame_index * self._samples_per_frame)
             data.append(ih.read(self._samples_per_frame))
         return self.task(data)
 
@@ -123,6 +150,14 @@ class CombineStreams(Task, CombineStreamsBase):
     method : bool, optional
         Whether ``task`` is a method (two arguments) or a function
         (one argument).  Default: inferred by inspection.
+    atol : `~astropy.units.Quantity`
+        Tolerance in units of time within which streams should be considered
+        aligned.
+    samples_per_frame : int, optional
+        Number of samples which should be fed to the function in one go.
+        If not given, by default the number from the first underlying file.
+        Useful mostly in case the stream readers have time offsets, since
+        the output stream will be shortened by an integer number of frames.
 
     See Also
     --------
@@ -131,8 +166,10 @@ class CombineStreams(Task, CombineStreamsBase):
     """
     # Override __init__ only to get rid of kwargs of Task, since these cannot
     # be passed on to ChangeSampleShapeBase anyway.
-    def __init__(self, ihs, task, method=None):
-        super().__init__(ihs, task, method=method)
+    def __init__(self, ihs, task, method=None, atol=1.*u.ns,
+                 samples_per_frame=None):
+        super().__init__(ihs, task, method=method, atol=atol,
+                         samples_per_frame=samples_per_frame)
 
 
 class Concatenate(CombineStreamsBase):
@@ -145,15 +182,23 @@ class Concatenate(CombineStreamsBase):
     axis : int
         Axis along which to combine the samples. Should be a sample
         axis and thus cannot be 0.
+    atol : `~astropy.units.Quantity`
+        Tolerance in units of time within which streams should be considered
+        aligned.
+    samples_per_frame : int, optional
+        Number of samples which should be fed to the function in one go.
+        If not given, by default the number from the first underlying file.
+        Useful mostly in case the stream readers have time offsets, since
+        the output stream will be shortened by an integer number of frames.
 
     See Also
     --------
     Stack : to stack streams along a new axis
     CombineStreams : to combine streams with a user-supplied function
     """
-    def __init__(self, ihs, axis=1):
+    def __init__(self, ihs, axis=1, atol=1.*u.ns, samples_per_frame=None):
         self.axis = axis
-        super().__init__(ihs)
+        super().__init__(ihs, atol=atol, samples_per_frame=samples_per_frame)
 
     def task(self, data):
         """Concatenate the pieces of data together."""
@@ -175,15 +220,23 @@ class Stack(CombineStreamsBase):
     axis : int
         New axis along which to stack the samples. Should be a sample
         axis and thus cannot be 0.
+    atol : `~astropy.units.Quantity`
+        Tolerance in units of time within which streams should be considered
+        aligned.
+    samples_per_frame : int, optional
+        Number of samples which should be fed to the function in one go.
+        If not given, by default the number from the first underlying file.
+        Useful mostly in case the stream readers have time offsets, since
+        the output stream will be shortened by an integer number of frames.
 
     See Also
     --------
     Concatenate : to concatenate streams along an existing axis
     CombineStreams : to combine streams with a user-supplied function
     """
-    def __init__(self, ihs, axis=1):
+    def __init__(self, ihs, axis=1, atol=1.*u.ns, samples_per_frame=None):
         self.axis = axis
-        super().__init__(ihs)
+        super().__init__(ihs, atol=atol, samples_per_frame=samples_per_frame)
 
     def task(self, data):
         """Stack the pieces of data."""
