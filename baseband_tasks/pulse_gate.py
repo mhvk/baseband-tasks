effeb539028049b70d3d0d1f0a564993b59742e0
Jing Luo <luojing1211@gmail.com>
2021-12-15 17:08:03 -0500

    Add more tests

--- baseband_tasks/pulse_gate.py
+++ baseband_tasks/pulse_gate.py
@@ -1,8 +1,8 @@
 """Functions to produce pulse gated data(only with the pulse part)
 """
 import numpy as np
+import astropy.units as u
 from .phases import PolycoPhase, PintPhase
-from .dispersion import DedisperseShift
 
 
 class GatePulse:
@@ -18,14 +18,22 @@ class GatePulse:
         can be an `~astropy.units.Quantity` with angular units or a regular
         array of float (in which case units of cycles are assumed).
 
-    gate : tuple
-        The start gate pulse phase and the end gate pulse phase
+    gate : list of two float numbers
+        The start gate pulse phase and the end gate pulse phase. The order of
+        the input gate is given by `(start_gate, end_gate)`. Both gates should
+        be in between [0, 1]. If the `start_gate` is bigger than the `end_gate`,
+        it assumes that the gate is from (start_gate, end_gate + 1)
     tol : float, optional
-        The tolarence of the pulse phaes gating. Default is 0.01
+        The tolarence of the pulse phaes gating. Tolarence can not be bigger
+        than 10% of the gate size. Default is 5% of the given gate size.
+    pulse_period: `~astropy.unit.Quantity` object, optional
+        Input pulse period. If not given, it will search from the input
+        callable `phase` object. If given, it has to be in the unit of time.
+
     """
-    def __init__(fh, phase, gate, tol=0.01, pulse_period=None):
+    def __init__(self, fh, phase, gate, tol=None, pulse_period=None):
         self.ih = fh
-        if not pulse_period: # Get pulse period from phase class
+        if pulse_period is None: # Get pulse period from phase class
             try:
                 self.pulse_period = 1.0 / phase.apparent_spin_freq(fh.start_time)
             except AttributeError:
@@ -33,16 +41,26 @@ class GatePulse:
                                  "class. Please provide a valid"
                                  " `pulse_period`.")
         else:
-            self.pulse_period = pulse_period
+            self.pulse_period = pulse_period.to(u.s)
 
-        self.samples_per_period = self.pulse_period * dedisperse_fh.sample_rate
+        self.gate = self.verify_gate(gate)
+        self.samples_per_period = self.pulse_period * self.ih.sample_rate
         self.phase_per_sample = 1.0 / self.samples_per_period
+        # Check the tolerence range, tolerance has to smaller than 10% of gate size
+        if tol is None:
+            tol =  (self.gate[1] - self.gate[0]) / 20
+        assert tol > 0
+        assert tol < (self.gate[1] - self.gate[0]) / 10
         # Compute the tolarence sample numbers
         self.tol_sample = tol / self.phase_per_sample
-        self.gate = gate
         self.pulse_offset = 0
 
-    def __call__(self, number_of_pulse):
+    def __call__(self):
+        pass
+
+    def next_nperiod_phase(self, number_of_pulse):
+        """Compute the pulse phase for next N pulse period
+        """
         # Compute the data time axis
         time_axis = (np.arange(0, self.samples_per_period * number_of_pulse,
                                tol_sample) / dedisperse_fh.time)
@@ -50,3 +68,11 @@ class GatePulse:
         pulse_phase = phase(time_axis)
         # Search the gate
         return pulse_phase
+
+    def verify_gate(self, gate):
+        assert len(gate) == 2
+        if gate[0] > gate[1]: #
+            gate[1] += 1
+        assert gate[1] - gate[0] < 1
+        # Normalize gate to 0 to 1
+        return np.array(gate) - np.modf(gate[0])[1]
--- baseband_tasks/tests/test_pulse_gating.py
+++ baseband_tasks/tests/test_pulse_gating.py
@@ -1,7 +1,14 @@
 """Test script for pulsar gating
 """
-import baseband_tasks.pulse_gate as pg
+import os
+import pytest
+import numpy as np
+import astropy.units as u
+from astropy.time import Time
+
+from baseband_tasks.pulse_gate import GatePulse
 from baseband_tasks.phases import PolycoPhase, Phase
+from baseband_tasks.generators import StreamGenerator
 
 
 test_data = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -12,15 +19,15 @@ class TestPulseGating:
         self.start_time = Time('2018-05-06T23:00:00', format='isot',
                                scale='utc')
         self.polyco_file = os.path.join(test_data, 'B1937_polyco.dat')
-        self.polyco = Polyco(self.polyco_file)
+        self.polyco = PolycoPhase(self.polyco_file)
         self.sample_rate = 128. * u.kHz
         self.shape = (164000, 2)
         self.gp_sample = 64000
         self.gp = StreamGenerator(self.make_giant_pulse,
                                   shape=self.shape, start_time=self.start_time,
                                   sample_rate=self.sample_rate,
-                                  samples_per_frame=1000, dtype=np.complex64,
-                                  frequency=300*u.MHz,
+                                  samples_per_frame=1000, dtype=np.float32,
+                                  frequency=[299.936, 300.064]*u.MHz,
                                   sideband=np.array((1, -1)))
 
     @classmethod
@@ -29,3 +36,13 @@ class TestPulseGating:
         do_gp = sh.tell() + np.arange(sh.samples_per_frame) == self.gp_sample
         data[...] = do_gp[:, np.newaxis]
         return data
+
+    @pytest.mark.parametrize('gate', [[0.2, 0.5], [1.3, 1.4], [0.8, 0.2],
+                                      [3.8, 3.2]])
+    @pytest.mark.parametrize('tol', [None, 0.001])
+    @pytest.mark.parametrize('pulse_period', [None, 1.6 * u.ms, 2*u.min])
+    def test_pulse_gate_build(self, gate, tol, pulse_period):
+        gate_pulse = GatePulse(self.gp, self.polyco, gate, tol, pulse_period)
+        assert gate_pulse.gate[0] == gate[0] - np.modf(gate[0])[1]
+        if gate[0] > gate[1]:
+            assert gate_pulse.gate[1] == gete[1] + 1 - np.modf(gate[0])[1]
